6c89f4549a71d702b426690eb1991591f2575069
Ty Hyten <ty.hyten@acumendigital.com>
2015-09-02 16:25:09 -0600

    updated file structure

--- app/album-favorites.js
+++ app/album-favorites.js
@@ -12,7 +12,7 @@ angular.module('AlbumFavorites', [
             {"id": 0, "artist": "Sharon Van Etten", "albumName": "I Don't Want to Let You Down", "year":"2015", "name": "Rock"},
             {"id": 1, "artist": "Nathaniel Rateliff", "albumName": "Nathaniel Rateliff", "year": "2015", "name": "Rock"},
             {"id": 2, "artist": "Gregory Alan Isakov", "albumName": "The Weatherman", "year": "2014", "name": "Folk"},
-            {"id": 3, "artist": "Kendrick Lamar", "albumName": "Butterfly", "year": "2015", "name": "Hip-Hop"},
+            {"id": 3, "artist": "Kendrick Lamar", "albumName": "PeanutButterfly", "year": "2015", "name": "Hip-Hop"},
             {"id": 4, "artist": "Meatloaf", "albumName": "Meatloaf", "year": "2015", "name": "Rock"},
             {"id": 5, "artist": "Shaq", "albumName": "Magic", "year": "2015", "name": "Hip-Hop"},
             {"id": 6, "artist": "Slayer", "albumName": "Spaghetti", "year": "2015", "name": "Rock"},
